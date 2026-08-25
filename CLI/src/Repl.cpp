32681be4bb01a1887083b3cc12e3e7809ba62fda
kleeedolinux <71413974+kleeedolinux@users.noreply.github.com>
2026-08-25 01:41:31 -0300

    Prevent Windows min macro expansion

--- CLI/src/Repl.cpp
+++ CLI/src/Repl.cpp
@@ -80,7 +80,12 @@ static const char* reset()
 #include <io.h>
 #include <fcntl.h>
 
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
 #include <windows.h>
 #endif
 
